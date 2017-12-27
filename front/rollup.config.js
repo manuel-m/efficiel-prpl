cd83f5b0e2bb5b4426653b613379fc4b09f1d6ed
Manuel M <discoride.dev@gmail.com>
2017-12-27 19:50:45 +0100

    improve prod and dev build

--- front/rollup.config.js
+++ front/rollup.config.js
@@ -1,7 +1,12 @@
 var surplus = require('../PR/rollup-plugin-surplus/index'), // [!] Pull request in course
     eslint = require('rollup-plugin-eslint'),
     resolve = require('rollup-plugin-node-resolve'),
-    uglify = require('rollup-plugin-uglify');
+    uglify = require('rollup-plugin-uglify'),
+    _tasks = [eslint(), surplus(), resolve({ extensions: ['.js', '.jsx'] })];
+
+if (process.env.NODE_ENV === 'production') {
+    _tasks.push(uglify());
+}
 
 export default {
     input: './front.main.index.js',
@@ -10,38 +15,5 @@ export default {
         format: 'iife',
         file: '../dist/front.main.bundle.js'
     },
-    plugins: [
-        eslint(),
-        surplus(),
-        resolve({ extensions: ['.js', '.jsx'] }),
-        uglify(
-            process.env.NODE_ENV === 'production'
-                ? undefined
-                : {
-                      compress: {
-                          sequences: true,
-                          properties: true,
-                          dead_code: true,
-                          drop_debugger: true,
-                          unsafe: false,
-                          conditionals: true,
-                          comparisons: true,
-                          evaluate: false, // [!] disable asm.js if true
-                          booleans: true,
-                          loops: true,
-                          unused: true,
-                          hoist_funs: true,
-                          hoist_vars: true,
-                          if_return: true,
-                          join_vars: true,
-                          side_effects: false,
-                          warnings: true
-                      },
-                      mangle: false,
-                      output: {
-                          beautify: true
-                      }
-                  }
-        )
-    ]
+    plugins: _tasks
 };
