66c21bb863fdee633f4589454ff57777dc7d04a3
Manuel M <discoride.dev@gmail.com>
2017-12-29 10:06:34 +0100

    improve sample

--- build.js
+++ build.js
@@ -7,6 +7,18 @@ var fs = require('fs-extra'),
     function INSURE_DIST() {
         fs.ensureDirSync(_dist_dir);
     },
+    function ASSETS_COPY() {
+        var _source = 'front/app/assets/css',
+            _dest = _build_dir + '/assets/css';
+        fs.ensureDirSync(_dest);
+
+        fconcat(
+            [_source + '/bootstrap.css', _source + '/app.css'],
+            _dest + '/style.css'
+        );
+
+        // fcopyDir('front/app/assets', _build_dir + '/assets');
+    },
     function JS_BUILD() {
         shell('rollup -c --environment build:production');
     },
@@ -16,12 +28,28 @@ var fs = require('fs-extra'),
 
         fwrite(
             _build_dir + '/index.html',
-            _index_html.replace('/*@script*/', _critical_js)
+            _index_html.replace(
+                '<!-- @script -->',
+                '<script>' + _critical_js + '</script>'
+            )
         );
     }
 ].forEach(function(task_) {
     task_();
 });
+
+function fconcat(sources_, destination_) {
+    var _concat = '';
+    sources_.forEach(function(source_) {
+        _concat += fread(source_);
+    });
+    fwrite(destination_, _concat);
+}
+
+function fcopyDir(source_, destination_) {
+    fs.removeSync(destination_);
+    fs.copySync(source_, destination_);
+}
 
 function fread(path_) {
     return fs.readFileSync(path_).toString();
--- front/app/js/2_addons/views/profile/profile.jsx
+++ front/app/js/2_addons/views/profile/profile.jsx
@@ -1,16 +1,27 @@
 export default function _profile_view(Surplus, context_) {
     return (
-        <div>
-            <pre>The profile view</pre>
-            <button
-                onclick={function() {
-                    context_.go('home');
-                }}
-            >
-                Back
-            </button>
+        <div class="container">
+            <h1>The profile view</h1>
 
-            <pre>{JSON.stringify(context_, undefined, 1)}</pre>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </p>
+                    <pre>{JSON.stringify(context_, undefined, 1)}</pre>
+                    <a
+                        href="#"
+                        class="btn btn-primary"
+                        onclick={function() {
+                            context_.go('home');
+                        }}
+                    >
+                        back
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
